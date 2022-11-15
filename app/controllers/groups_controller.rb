961ba372c0d44772c42ffaf3ff21a41b6eb51198
jaawadalishah5 <livespotless@gmail.com>
2022-11-15 15:48:57 +0500

    without expenses

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,15 +5,27 @@ class GroupsController < ApplicationController
     end
 
     def create
-        @group = Group.new(group_params.merge(user: current_user))     
-
+        # @group = Group.new(group_params.merge(user: current_user)) 
+        # @test = User.where(email: email_params["emails"]) 
+        @group = Group.new(params.require(:group).permit(:name))     
+        
+        @user = current_user   
+        @group.users << @user
+        email_params["emails"].split(",").each do |e|
+            @group.users << User.where(email: e)
+        end
+        # @group.users << @test
         if @group.save
             redirect_to root_path     
         end
     end
 
-    def group_params
-        params.require(:group).permit(:name)
+    def email_params
+        params.require(:group).permit(:emails)
+    end
+
+    def show
+        
     end
 
      
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+  has_and_belongs_to_many :users
+
+  def index
+    
+  end
+  
 end
